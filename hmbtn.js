a7e2d4375eb3d41a47a29de906ce5df003c3bbd2
StTPotW <mesayan19@gmail.com>
2022-11-22 11:08:27 +0530

    filter done

--- hmbtn.js
+++ hmbtn.js
@@ -1,10 +1,15 @@
 const primaryNav = document.querySelector("#primary-navigation");
 const navToggle = document.querySelector(".hambgrbtn");
-// const blur = document.querySelector(".container");
+const blur = document.querySelector("#logo");
+const blur1 = document.querySelector(".front");
+const blur2 = document.querySelector(".content-article");
+
 
 navToggle.addEventListener('click', ()=>{
     const visibility = primaryNav.getAttribute("data-visible");
-    // blur.classList.toggle("active");
+    blur.classList.toggle("active");
+    blur1.classList.toggle("active");
+    blur2.classList.toggle("active");
     if (visibility === "false"){
         primaryNav.setAttribute("data-visible", "true");
         navToggle.setAttribute("aria-expanded", "true");
